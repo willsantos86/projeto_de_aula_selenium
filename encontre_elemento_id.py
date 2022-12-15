382cc87707c6a410579c1d639debeeb88c0198b3
William Santos <willsantos.edf@gmail.com>
2022-12-15 14:00:10 -0300

    Aulas de como encontrar elementos nas paginas de html.

---
+++ encontre_elemento_id.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+def iniciar_driver():
+    chrome_options = Options()
+    arguments = ['--lang=pt-BR', '--window-size=800,600', '--incognito']
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1,
+
+    })
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()), options=chrome_options)
+
+    return driver
+
+
+driver = iniciar_driver()
+driver.get('https://cursoautomacao.netlify.app/')
+
+botao = driver.find_element(By.ID, 'buttonalerta')
+botoes = driver.find_elements(By.ID, 'buttonalerta')
+
+if botao is not None:
+    print('Botao encontrado!')
+
+if botoes is not None:
+    print('Botoes encontrados')
+
+
+input('')
---
+++ encontre_elemento_nome.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+def iniciar_driver():
+    chrome_options = Options()
+    arguments = ['--lang=pt-BR', '--window-size=800,600', '--incognito']
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1,
+
+    })
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()), options=chrome_options)
+
+    return driver
+
+
+driver = iniciar_driver()
+driver.get('https://cursoautomacao.netlify.app/')
+
+campo_nome = driver.find_element(By.NAME, 'seu-nome')
+radio_buttons = driver.find_elements(By.NAME, 'exampleRadios')
+
+if campo_nome is not None:
+    print('Campo nome encontrado!')
+
+if radio_buttons is not None:
+    print('Radios buttons encontrados')
+
+
+input('')
