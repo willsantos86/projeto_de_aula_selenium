d05c1f3e58ce1025e97eb5e3a6b6b14248d78441
William Santos <willsantos.edf@gmail.com>
2022-12-15 23:54:08 -0300

    Aulas de como encontrar elementos nas paginas de html.

---
+++ encontre_elemento_texto.py
@@ -0,0 +1,39 @@
+from tkinter import Radiobutton
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+def iniciar_driver():
+    chrome_options = Options()
+    arguments = ['--lang=pt-BR', '--window-size=800,600', '--incognito']
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1,
+
+    })
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()), options=chrome_options)
+
+    return driver
+
+
+driver = iniciar_driver()
+driver.get('https://cursoautomacao.netlify.app/')
+
+link_home = driver.find_element(By.LINK_TEXT, 'Home')
+link_desafio = driver.find_element(By.PARTIAL_LINK_TEXT, 'Des')
+
+if link_home is not None:
+    print('Link home encontrado')
+if link_desafio is not None:
+    print('Link desafio encontrado')
+
+input('')
+driver.close()
