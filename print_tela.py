4cc30c2848ee5b8e2d37b9ce0d790b67e5a1b630
William Santos <willsantos.edf@gmail.com>
2022-12-19 15:21:04 -0300

    Adicionando o arquivo de como interagir com, checkbox, radio e dropdown.

--- print_tela.py
+++ print_tela.py
@@ -1,16 +1,14 @@
-from tabnanny import check
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from time import sleep
-from selenium.webdriver.support.select import Select
 
 
 def iniciar_driver():
     chrome_options = Options()
-    arguments = ['--lang=pt-BR', '--window-size=1400,1050', '--incognito']
+    arguments = ['--lang=pt-BR', '--window-size=800,1000', '--incognito']
     for argument in arguments:
         chrome_options.add_argument(argument)
 
@@ -27,23 +25,7 @@ def iniciar_driver():
 
 
 driver = iniciar_driver()
-
-
-# navegar até o site
-driver.get('https://cursoautomacao.netlify.app/desafios.html')
-sleep(5)
-#driver.execute_script("window.scrollTo(0, 1600)")
-#sleep(2)
-paises = driver.find_element(By.XPATH, "//select[@id='paisesselect']")
-opcoes = Select(paises)
-
-# Selecionar pelo valor
-opcoes.select_by_value('estadosunidos')
-sleep(2)
-# Selecionar pelo texto
-opcoes.select_by_index(3)
-sleep(2)
-opcoes.select_by_visible_text("Chille")
-
+driver.get('https://cursoautomacao.netlify.app')
+driver.save_screenshot('tela.jpg')
 input('')
 driver.close()
